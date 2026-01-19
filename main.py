7a432715f52270410b997ad2a43839566d294654
uchaudhry <ucopper4@gmail.com>
2026-01-19 15:16:46 -0600

    tree-prob

--- main.py
+++ main.py
@@ -98,3 +98,119 @@
 #             head,reversedHead = nextH, nextR
 
 # ---- End -----
+
+# Problem: Maximum Depth of Binary Tree https://neetcode.io/problems/depth-of-binary-tree/question
+
+# Solution with recursive dfs
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+#         nodes = max(self.maxDepth(root.left), self.maxDepth(root.right))
+#         return 1 + nodes
+        
+        
+# iterative dfs
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         stack = [[root, 1]]
+#         res = 0
+
+#         while stack:
+#             node, depth = stack.pop()
+
+#             if node:
+#                 res = max(res, depth)
+#                 stack.append([node.left, depth + 1])
+#                 stack.append([node.right, depth + 1])
+#         return res
+
+# breadth first search
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         q = deque()
+#         if root:
+#             q.append(root)
+
+#         level = 0
+#         while q:
+#             for i in range(len(q)):
+#                 node = q.popleft()
+#                 if node.left:
+#                     q.append(node.left)
+#                 if node.right:
+#                     q.append(node.right)
+#             level += 1
+#         return level
+
+# Problem: Subtree of Another Tree: 
+# Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and 
+# node values of subRoot and false otherwise. It's tricky with conditionals on the root and subroot
+
+# Where m is the number of nodes in subRoot and n is the number of nodes in root
+
+# time - O(m * n) 
+# space - O(m + n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def serialize(self, root: Optional[TreeNode]) -> str:
+#         if root == None:
+#             return "$#"
+
+#         return ("$" + str(root.val) + self.serialize(root.left) + self.serialize(root.right))
+
+#     def z_function(self, s: str) -> list:
+#         z = [0] * len(s)
+#         l, r, n = 0, 0, len(s)
+#         for i in range(1, n):
+#             if i <= r:
+#                 z[i] = min(r - i + 1, z[i - l])
+#             while i + z[i] < n and s[z[i]] == s[i + z[i]]:
+#                 z[i] += 1
+#             if i + z[i] - 1 > r:
+#                 l, r = i, i + z[i] - 1
+#         return z
+
+#     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+#         serialized_root = self.serialize(root)
+#         serialized_subRoot = self.serialize(subRoot)
+#         combined = serialized_subRoot + "|" + serialized_root
+
+#         z_values = self.z_function(combined)
+#         sub_len = len(serialized_subRoot)
+
+#         for i in range(sub_len + 1, len(combined)):
+#             if z_values[i] == sub_len:
+#                 return True
+#         return False
